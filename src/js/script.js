75a40e911380442ae9acc423105e4be7fab36c9f
DahianaGL <dgavirialombana@gmail.com>
2024-11-01 23:03:03 -0500

    Footer completed
    Final adjustments to the footer

--- src/js/script.js
+++ src/js/script.js
@@ -86,15 +86,7 @@ lightbox.addEventListener('click', () => {
 
 
 /* SCRIPT FOOTER */
-function GetMap() {
-    var map = new Microsoft.Maps.Map('#footer-map-location', {
-        credentials: 'TU_API_KEY',
-        center: new Microsoft.Maps.Location(6.2442, -75.5812),
-        zoom: 12
-    });
-}
-
-document.getElementById('footer-supportForm').addEventListener('submit', function(event) {
+document.getElementById('footer-Form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const name = document.getElementById('footer-name').value;
